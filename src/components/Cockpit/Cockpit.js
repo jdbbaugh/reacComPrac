5e679ead84187f1546eb5443d5ddba4be9ee635a
Joseph Baugh <jbaugh78@msn.com>
2019-07-26 16:42:08 -0500

    to appBuildOne

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,28 +1,54 @@
-import React from "react";
-import classes from "./Cockpit.css";
+import React, { useEffect, useRef, useContext } from "react";
 
-const cockpit = props => {
+import classes from "./Cockpit.css";
+import AuthContext from "../../context/auth-context";
+
+function Cockpit(props) {
+  const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext);
+
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect");
+    toggleBtnRef.current.click();
+    setTimeout(() => {
+      // alert("saved data to cloud");
+    }, 1000);
+    return () => {
+      console.log("[Cockpit.js] cleanup work in useEffect");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("[Cockpit.js] 2nd useEffect");
+    return () => {
+      console.log("[Cockpit.js] cleanup work in 2nd useEffect");
+    };
+  });
+
   const assignedClasses = [];
   let btnClass = "";
   if (props.showPersons) {
     btnClass = classes.Red;
   }
-  if (props.persons.length <= 2) {
+  if (props.personsLength <= 2) {
     assignedClasses.push(classes.red);
   }
-  if (props.persons.length <= 1) {
+  if (props.personsLength <= 1) {
     assignedClasses.push(classes.bold);
   }
 
   return (
     <div className={classes.Cockpit}>
-      <h1>REact</h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>this is really working</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Person View
       </button>
+      {<button onClick={authContext.login}>Log in</button>}
     </div>
   );
-};
+}
 
-export default cockpit;
+export default React.memo(Cockpit);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import withClass from "../hoc/withClass";
+import AuthContext from "../context/auth-context";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
   state = {
     persons: [
       { id: 1, name: "Maximilian", age: 28 },
-      { id: 12, name: "Manu", age: 29 },
+      { id: 12, name: "Manu", age: "29" },
       { id: 13, name: "Stephanie", age: 26 }
     ],
     otherState: "some other value",
-    showPersons: false
+    showPersons: false,
+    showCockpit: true,
+    changeCounter: 0,
+    authenticated: false
   };
+
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[App.js] getDerivedStateFromProps", props);
+  //   return state;
+  // }
+
+  // componentWillMount() {
+  //   console.log("[App.js] componentWillMount");
+  // }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js] shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("[App.js] componentDidUpdate");
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -28,7 +59,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter
+      };
+    });
   };
 
   deletePersonHandler = personIndex => {
@@ -42,7 +78,12 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({ authenticated: true });
+  };
+
   render() {
+    console.log("[App.js render]");
     let persons = null;
 
     if (this.state.showPersons) {
@@ -51,18 +92,36 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
         />
       );
     }
 
     return (
       <div className="App">
-        <Cockpit
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-        />
-        {persons}
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}
+        >
+          Remove Cockpit
+        </button>
+        <AuthContext.Provider
+          value={{
+            authenticated: this.state.authenticated,
+            login: this.loginHandler
+          }}
+        >
+          {this.state.showCockpit && (
+            <Cockpit
+              title={this.props.title}
+              showPersons={this.state.showPersons}
+              personsLength={this.state.persons.length}
+              clicked={this.togglePersonsHandler}
+            />
+          )}
+          {persons}
+        </AuthContext.Provider>
       </div>
     );
   }
